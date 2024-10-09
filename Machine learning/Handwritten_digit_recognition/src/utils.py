bf6dbd8cead252562ce2dc2ef78ccbd1a44c2bcf
Ieee-Ras-Muj <ras.ieee.muj@gmail.com>
2024-10-09 06:45:42 +0530

    Update utils.py

--- Machine learning/Handwritten_digit_recognition/src/utils.py
+++ Machine learning/Handwritten_digit_recognition/src/utils.py
@@ -1,1 +1,7 @@
+import numpy as np
+
+def preprocess_data(x):
+    """Normalize and reshape data for the CNN model."""
+    x = x / 255.0  # Normalize
+    return np.expand_dims(x, -1)  # Add a channel dimension
 
