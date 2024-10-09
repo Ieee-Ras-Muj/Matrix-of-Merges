b3ca45eaa9e7d97b506b73234b3ddd3bce0daccc
Ieee-Ras-Muj <ras.ieee.muj@gmail.com>
2024-10-09 06:44:35 +0530

    Update model.py

--- Machine learning/Handwritten_digit_recognition/src/model.py
+++ Machine learning/Handwritten_digit_recognition/src/model.py
@@ -1,1 +1,17 @@
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Conv2D, MaxPooling2D, Flatten, Dense, Dropout
+
+def build_model():
+    model = Sequential([
+        Conv2D(32, (3, 3), activation='relu', input_shape=(28, 28, 1)),
+        MaxPooling2D((2, 2)),
+        Conv2D(64, (3, 3), activation='relu'),
+        MaxPooling2D((2, 2)),
+        Flatten(),
+        Dense(128, activation='relu'),
+        Dropout(0.5),
+        Dense(10, activation='softmax')
+    ])
+    model.compile(optimizer='adam', loss='sparse_categorical_crossentropy', metrics=['accuracy'])
+    return model
 
