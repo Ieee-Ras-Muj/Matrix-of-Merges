db5ba5919862a395c474aa74074ba9a0d7767bc2
shrivatsatrivedi <shrivatsatrivedi689@gmail.com>
2024-10-16 14:41:03 +0530

    Certificate

--- certificate_generator/generator.py
+++ certificate_generator/generator.py
@@ -32,7 +32,7 @@ def make_cert(name):
     print(f"Printing certificate for: {name}")
 
 
-names = ["Dhruv Kunzru"]
+names = ["Dhruv Kunzru","Shrivatsa Trivedi"]
 
 for name in names:
     make_cert(name)
