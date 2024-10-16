0efe34716efbd1e12de7a40e894644dd9e41b484
Priyanchew <priyanshu6beta@gmail.com>
2024-10-16 14:33:15 +0530

    Hmmm

--- certificate_generator/generator.py
+++ certificate_generator/generator.py
@@ -32,7 +32,7 @@ def make_cert(name):
     print(f"Printing certificate for: {name}")
 
 
-names = ["Dhruv Kunzru"]
+names = ["Dhruv Kunzru", "Priyanshu Choudhary", "Eren Yeager"]
 
 for name in names:
     make_cert(name)
